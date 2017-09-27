7687948ccb03512d49d06337b5112ea7c9417afd
Unknown <flo.erwan@gmail.com>
2017-09-27 17:44:50 +0200

    Test Junit v1.1

---
+++ test JUnit/src/test/java/rest_assured/test/ConfigureRestAssured.java
@@ -0,0 +1,35 @@
+package rest_assured.test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.junit.BeforeClass;
+
+import io.restassured.RestAssured;
+
+public class ConfigureRestAssured {
+
+	@BeforeClass
+	public static void configureRestAssured() throws FileNotFoundException, IOException {
+
+		// get properties file path
+		String filename = "restAssured.properties";
+		InputStream filenamePath = ConfigureRestAssured.class.getClassLoader().getResourceAsStream(filename);
+		
+		// load properties file
+		Properties propRestAssured = new Properties();
+		propRestAssured.load(filenamePath);
+		
+		// set property value for Rest Assured config
+		RestAssured.baseURI = propRestAssured.getProperty("baseURI");
+		RestAssured.port = Integer.parseInt(propRestAssured.getProperty("port"));
+		RestAssured.basePath = propRestAssured.getProperty("basePath");
+
+		// Enable logging of both the request and the response if REST Assureds
+		// test validation fails
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+	}
+}
